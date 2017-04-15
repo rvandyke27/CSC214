5ce8860e64ceb27f3afee9abeee693d9a2ebe13a
rvandyke27 <rvandyke27@gmail.com>
2017-04-15 19:12:42 -0400

    so close
    still need to fix recyclerview onclick and do documentation

--- Assignment 8.5/app/src/main/java/com/csc214/rvandyke/assignment6application/TeamRecyclerFragment.java
+++ Assignment 8.5/app/src/main/java/com/csc214/rvandyke/assignment6application/TeamRecyclerFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -93,6 +94,7 @@ public class TeamRecyclerFragment extends Fragment {
             FragmentManager manager = getFragmentManager();
             TeamDescriptionDialog dialog = TeamDescriptionDialog.newInstance(mTeam);
 
+            Log.d(TAG, "onClick called");
             dialog.show(manager, "TeamDetail");
         }
     } //end class TeamViewHolder
