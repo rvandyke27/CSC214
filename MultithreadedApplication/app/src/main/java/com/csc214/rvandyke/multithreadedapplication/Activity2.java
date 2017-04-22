cdcfef46b50738f875380de84ea6c895a687302e
rvandyke27 <rvandyke27@gmail.com>
2017-04-21 21:27:18 -0400

    implemented activity 2

--- MultithreadedApplication/app/src/main/java/com/csc214/rvandyke/multithreadedapplication/Activity2.java
+++ MultithreadedApplication/app/src/main/java/com/csc214/rvandyke/multithreadedapplication/Activity2.java
@@ -1,5 +1,6 @@
 package com.csc214.rvandyke.multithreadedapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -37,7 +39,7 @@ public class Activity2 extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Log.d(TAG, "Square Root Requested");
-                //TODO
+                new CalculateSqrt().execute(mLongInput.getText().toString());
             }
         });
 
@@ -46,7 +48,7 @@ public class Activity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "Largest Prime Requested");
-                //TODO
+                new findPrime().execute(mLongInput.getText().toString());
             }
         });
 
@@ -57,5 +59,84 @@ public class Activity2 extends AppCompatActivity {
         super.onDestroy();
         Log.d(TAG, "onDestroy called");
     } //onDestroy()
+
+    private class CalculateSqrt extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params){
+            Log.d(TAG, getString(R.string.calculatesqrt));
+            Long val = Long.valueOf(0);
+            try{
+                val = Long.valueOf(params[0]);
+            } catch(NumberFormatException e){
+                return "";
+            }
+            if(val >= 2) {
+                return Double.toString(Math.sqrt(val));
+            }
+            else{
+                return "";
+            }
+        } //doInBackground()
+
+        @Override
+        protected void onPostExecute(String s){
+            if(!s.equals("")) {
+                mOutput.setText(getString(R.string.sqrt) + s);
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Invalid input!", Toast.LENGTH_LONG).show();
+            }
+        } //onPostExecute()
+
+    } //end class CalculateSqrt
+
+    private class findPrime extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... params){
+            long output = 0;
+            long val = 0;
+
+            try{
+                val = Long.valueOf(params[0]);
+            } catch(NumberFormatException e) {
+                return "";
+            }
+            if(val >= 2) {
+                for (long i = 0; i <= val; i++) {
+                    if (isPrime(i)) {
+                        output = i;
+                    }
+                }
+                return Double.toString(output);
+            }
+            else{
+                return "";
+            }
+        } //doInBackground()
+
+        @Override
+        protected void onPostExecute(String s){
+            if(!s.equals("")) {
+                mOutput.setText(getString(R.string.largestPrime) + s);
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Invalid input!", Toast.LENGTH_LONG).show();
+            }
+        } //onPostExecute()
+    } //end class findPrime
+
+    public boolean isPrime(long n){
+        if(n%2 == 0){
+            return false;
+        }
+        else{
+            for(int i=3; i*i<=n; i +=2){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    } //isPrime()
 
 } //end class Activity2
